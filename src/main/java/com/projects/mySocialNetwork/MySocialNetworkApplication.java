1d581d48326c5bf51e4bcf7b2f14e30d1ebfc27e
vladimirs1989 <sitnik_vladimir89@mail.ru>
2022-08-25 14:15:11 +0300

    create project with spring io

---
+++ src/main/java/com/projects/mySocialNetwork/MySocialNetworkApplication.java
@@ -0,0 +1,13 @@
+package com.projects.mySocialNetwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySocialNetworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySocialNetworkApplication.class, args);
+	}
+
+}
