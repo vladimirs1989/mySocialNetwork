66e3a136ebdefdddaab1e7eaf48ebd883d781d0d
vladimirs1989 <sitnik_vladimir89@mail.ru>
2022-09-03 17:05:21 +0300

    roles and access

---
+++ src/main/java/com/projects/mySocialNetwork/controller/UserController.java
@@ -0,0 +1,65 @@
+package com.projects.mySocialNetwork.controller;
+
+import com.projects.mySocialNetwork.entity.Role;
+import com.projects.mySocialNetwork.entity.User;
+import com.projects.mySocialNetwork.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Arrays;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Controller
+@RequestMapping("/user")
+@PreAuthorize("hasAuthority('ADMIN')") //проверяет наличие прав пользователя,
+                                        // работает только с аннотацией @EnableGlobalMethodSecurity(prePostEnabled = true) в SecurityConfig
+public class UserController {
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping
+    public String userList(Model model) {
+        model.addAttribute("users", userRepository.findAll());
+        return "userList";
+    }
+
+    @GetMapping("{user}")
+    public  String userEditForm(@PathVariable User user, Model model){
+        model.addAttribute("user", user);
+        model.addAttribute("roles", Role.values());
+       return "userEdit";
+    }
+
+    @PostMapping
+    public String userSave(
+            @RequestParam String username,
+            @RequestParam Map<String, String> form,
+            @RequestParam("userId") User user){
+
+        user.setUsername(username);
+
+        Set<String> roles = Arrays.stream(Role.values())
+                .map(Role::name)
+                .collect(Collectors.toSet());
+
+        user.getRoles().clear();
+
+        for (String key : form.keySet()){
+            if(roles.contains(key)){
+                user.getRoles().add(Role.valueOf(key));
+            }
+        }
+
+        userRepository.save(user);
+        return "redirect:/user";
+    }
+}
--- src/main/java/com/projects/mySocialNetwork/entity/Role.java
+++ src/main/java/com/projects/mySocialNetwork/entity/Role.java
@@ -3,7 +3,7 @@ package com.projects.mySocialNetwork.entity;
 import org.springframework.security.core.GrantedAuthority;
 
 public enum Role implements GrantedAuthority {
-    USER;
+    USER, ADMIN;
 
     @Override
     public String getAuthority() {
